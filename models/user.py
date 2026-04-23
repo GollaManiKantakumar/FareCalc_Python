5366d73f1d26f63f4ca91ae958133113866a8755
Golla Manikanta Kumar <manikantakumargolla4@gmail.com>
2026-04-23 21:26:20 +0530

    Removed all files from repository

--- models/user.py
+++
@@ -1,6 +0,0 @@
-class User:
-    def __init__(self, acc_no, name, password):
-        self.acc_no = acc_no
-        self.name = name
-        self.password = password
-        self.transactions = []
