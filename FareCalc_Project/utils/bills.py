3d0119cd7c857d77cc493ecf68f09c33b8b50c88
Golla Manikanta Kumar <manikantakumargolla4@gmail.com>
2026-04-23 21:48:28 +0530

    Cleared repository (all files removed)

--- FareCalc_Project/utils/bills.py
+++
@@ -1,8 +0,0 @@
-def print_bill(ride):
-    print("\n----- BILL -----")
-    print("Distance:", ride.km, "km")
-    print("Vehicle:", ride.vehicle)
-    print("Time:", ride.hour)
-    print("Amount:", ride.amount)
-    print("Status:", "Paid" if ride.paid else "Unpaid")
-    print("----------------\n")
