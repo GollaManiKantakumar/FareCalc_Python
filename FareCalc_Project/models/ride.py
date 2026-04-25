49e46e558d619b097c3eb3297157631b1bb3cb7c
GollaManiKantakumar <manikantakumargolla4@gmail.com>
2026-04-25 09:58:53 +0530

    Delete FareCalc_Project directory

--- FareCalc_Project/models/ride.py
+++
@@ -1,7 +0,0 @@
-class Ride:
-    def __init__(self, km, vehicle, hour, amount, paid=False):
-        self.km = km
-        self.vehicle = vehicle
-        self.hour = hour
-        self.amount = amount
-        self.paid = paid
